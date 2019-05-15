93ae47c68c4eac4c728b5da9f97f10e17f2ea69d
M_Ishikawa <ishikawam@nifty.com>
2019-05-15 21:03:51 +0900

    getUser

--- src/BoxUser.php
+++ src/BoxUser.php
@@ -9,4 +9,9 @@ trait BoxUser {
 		$url = $this->api_url . "/users/me";
 		return $this->get($url);
 	}
+
+	public function getUser($user_id) {
+		$url = $this->api_url . "/users/$user_id";
+		return $this->get($url);
+	}
 }
